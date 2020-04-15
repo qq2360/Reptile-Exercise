ba87699156604c8f86daff786bfb89de298120fe
qq2360 <38821508+qq2360@users.noreply.github.com>
2020-04-15 15:23:47 +0800

    Now you can use the search function

--- Pixivic.py
+++ Pixivic.py
@@ -39,6 +39,7 @@ async def real_download(task_list):
                                     await image.write(await response.content.read())
                             except:
                                 os.remove(filePath)
+                                print("Unable to download file or write file to disk.Path:" + filePath)
                         else:
                             os.mkdir(downloadDir)
                             await real_download(task_list)
@@ -54,40 +55,55 @@ def download(task_list):
     return
 
 
-async def main(date, which_mode, page_v, where_start, where_stop):
-    where_start = int(where_start)
-    where_stop = int(where_stop)
+async def main(is_search_mode, search_name, date, which_mode, page_v, where_start, where_stop):
     image_list = []
-    header = {
-        'Origin': 'https://pixivic.com',
-        'Referer': 'https://pixivic.com/?VNK=d2x231f3',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/74.0.3729.131 Safari/537.36'
-    }
+    if is_search_mode:
+        header = {
+            'Origin': 'https://pixivic.com',
+            'Referer': 'https://pixivic.com/keywords?tag='+search_name+'&illustType=illust',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+        }
+    else:
+        header = {
+            'Origin': 'https://pixivic.com',
+            'Referer': 'https://pixivic.com/?VNK=d2x231f3',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+        }
     if not (where_start is None or where_stop is None):
+        where_start = int(where_start)
+        where_stop = int(where_stop)
         if where_start <= 0 or where_stop <= 0:
             raise ValueError("Invalid start/stop index.")
         for next_page in range(where_start, where_stop + 1):
-            tmp_list = get_image_info(await get_resource_json(date, which_mode, next_page, header))
+            tmp_list = get_image_info(
+                await get_resource_json(is_search_mode, search_name, date, which_mode, next_page, header))
             if not tmp_list:
-                print("the last page is "+str(next_page))
+                print("the last page is " + str(next_page))
                 break
             image_list += tmp_list
         size = len(image_list) // 5
         for tasks in [image_list[i:i + size] for i in range(0, len(image_list), size)]:
             multiprocessing.Process(target=download, args=(tasks,)).start()
     else:
-        image_list = get_image_info(await get_resource_json(date, which_mode, page_v, header))
+        image_list = get_image_info(
+            await get_resource_json(is_search_mode, search_name, date, which_mode, page_v, header))
         size = len(image_list) // 5
         for tasks in [image_list[i:i + size] for i in range(0, len(image_list), size)]:
             multiprocessing.Process(target=download, args=(tasks,)).start()
     return
 
 
-async def get_resource_json(date, which_mode, page_v, header):
-    api_url = "https://api.pixivic.com/ranks?page=" + str(page_v) + "&date=" + str(date) + "&mode=" + str(
-        which_mode) + "&pageSize=90"
+async def get_resource_json(is_search_mode, search_name, date, which_mode, page_v, header):
+    if is_search_mode:
+        api_url = "https://api.pixivic.com/illustrations?illustType=illust&searchType=original&maxSanityLevel=6&page=" + str(
+            page_v) + "&keyword=" + search_name + "&pageSize=30"
+    else:
+        api_url = "https://api.pixivic.com/ranks?page=" + str(page_v) + "&date=" + str(date) + "&mode=" + str(
+            which_mode) + "&pageSize=30"
     async with aiohttp.ClientSession() as session:
         async with session.get(api_url, headers=header) as response:
+            if not (code:=response.status) == 200:
+                raise RuntimeError("API Request code:" + str(code))
             resource_json = await response.json()
     return resource_json
 
@@ -122,6 +138,7 @@ def get_image_info(json):
     try:
         data = json['data']
     except KeyError:
+        print("The last page has been reached.")
         return []
     image_list = []
     for image_message in data:
@@ -143,30 +160,33 @@ if __name__ == '__main__':
     mode = None
     start = None
     stop = None
+    search = None
     isAutoSetTime = True
     UseDefaultMode = True
+    isSearchMode = False
     page = 1
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hd:m:y:p:",
-                                   ["day=", "month=", "year=", "mode=", "pages=", "start=", "stop="])
+                                   ["day=", "month=", "year=", "mode=", "pages=", "start=", "stop=", 'search='])
     except getopt.GetoptError as e:
         print(e.msg)
         exit(1)
     for opt, arg in opts:
         if opt == "-h":
             print('Usage:\n \
-    -h    Print this message.\n \
-    -d <value>   Set resource day.\n \
-    -m <value>   Set resource month.\n \
-    -y <value>   Set resource year.\n \
-    -p <value> Set resource page.\n \
-    --day=<value> See -d.\n \
-    --month=<value> See -m.\n \
-    --year=<value> See -y.\n \
-    --mode=<value> Set get resource mode.Value must is one of these:day,week,month.If check value failed,try to use default config.\n \
-    --page=<value> See -p \
-    --start=<value> Set the page to start downloading \
-    --stop=<value> Set the page to stop downloading')
+    -h                  Print this message.\n \
+    -d <value>          Set resource day.\n \
+    -m <value>          Set resource month.\n \
+    -y <value>          Set resource year.\n \
+    -p <value>          Set resource page.\n \
+    --day=<value>       See -d.\n \
+    --month=<value>     See -m.\n \
+    --year=<value>      See -y.\n \
+    --mode=<value>      Set get resource mode.Value must is one of these:day,week,month.If check value failed,it will raise a ValueError.\n \
+    --page=<value>      See -p\n \
+    --start=<value>     Set the page to start downloading\n \
+    --stop=<value>      Set the page to stop downloading\n \
+    --search=<value>    Set the resource file you want to search.')
             exit(0)
         elif opt in ("-d", "--day"):
             day = arg
@@ -186,6 +206,9 @@ if __name__ == '__main__':
             start = arg
         elif opt in '--stop':
             stop = arg
+        elif opt in '--search':
+            search = arg
+            isSearchMode = True
     loop = asyncio.get_event_loop()
     for date_object in (day, month, year):
         if not isAutoSetTime and date_object is None:
@@ -199,7 +222,7 @@ if __name__ == '__main__':
                     mode = ModeEnum.DAY.value
                 else:
                     raise ValueError('Mode is invalid.')
-            loop.run_until_complete(main(auto_set_time(), mode, page, start, stop))
+            loop.run_until_complete(main(isSearchMode, search, auto_set_time(), mode, page, start, stop))
             break
     else:
-        loop.run_until_complete(main(format_date(year, month, day), mode, page, start, stop))
+        loop.run_until_complete(main(isSearchMode, search, format_date(year, month, day), mode, page, start, stop))
